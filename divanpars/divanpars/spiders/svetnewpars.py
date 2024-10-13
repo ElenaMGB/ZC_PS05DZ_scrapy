faf9ce2d0083af0cc1ed63c7772ffbfb56266f9b
ElenaM <eldev24@gmail.com>
2024-10-13 16:43:45 +0500

    svetnewpars

--- divanpars/divanpars/spiders/svetnewpars.py
+++ divanpars/divanpars/spiders/svetnewpars.py
@@ -1,10 +1,15 @@
 import scrapy
 
-
 class SvetnewparsSpider(scrapy.Spider):
     name = "svetnewpars"
     allowed_domains = ["https://divan.ru"]
     start_urls = ["https://www.divan.ru/ekaterinburg/category/svet"]
 
     def parse(self, response):
-        pass
+        svetilniks = response.css('div._Ud0k')
+        for svetilnik in svetilniks:
+            yield {
+            'name' : svetilniks.css('div.lsooF span::text').get(),
+            'price' : svetilniks.css('div.pY3d2 span::text').get(),
+            'url' : svetilniks.css('a').attrib['href']
+            }
